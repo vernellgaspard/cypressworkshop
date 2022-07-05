e3a4e2f61d113e1af1a9606278ffc8933326a462
vernellgaspard <107004474+vernellgaspard@users.noreply.github.com>
2022-07-05 08:20:36 -0400

    add: linter support and cypress configs

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 5000,
+  video: false,
+  chromeWebSecurity: false,
+  retries: 3,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+
   e2e: {
+    baseUrl: 'https://demoqa.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
